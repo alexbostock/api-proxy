6a0a4bb4c6be0fb5e8cc2d5e63fa1820632ff942
alexbostock <alex@alexbostock.co.uk>
2019-12-06 12:36:21 +0000

    Build basic proxy for RTT API

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ if (require('dotenv').config().error) {
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT_NUMBER;
+const axios = require('axios');
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+const port = process.env.PORT_NUMBER;
+if (process.env.BEHIND_REVERSE_PROXY) {
+    app.set('trust proxy', true);
+}
+
+app.get('/', (req, res) => {
+    axios.get('https://api.rtt.io/api/v1/json/search/CBG', {
+        auth: {
+            username: process.env.RTT_USERNAME,
+            password: process.env.RTT_PASSWORD,
+        },
+    })
+        .then((api_res) => {
+            console.log(api_res.status);
+            res.send(api_res.data);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Failed to fetch from Realtime Trains API');
+        });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
