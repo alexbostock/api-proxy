7bf79fc997a8008e45b0472153ecf3ec25b03bdc
alexbostock <alex@alexbostock.co.uk>
2019-12-06 13:35:17 +0000

    Discard unwanted information from RTT

--- trains/index.js
+++ trains/index.js
@@ -22,13 +22,17 @@ function fetchRTT(req, res, arrivals) {
         .then((api_res) => {
             switch(api_res.status) {
             case 200:
-                res.send(api_res.data);
+                res.send(trimData(api_res.data));
+                break;
             case 404:
                 res.status(400).send("Station not found");
+                break;
             case 500:
                 res.status(500).send("RTT API gave error 500");
+                break;
             case 429:
                 res.status(429).send("Rate limit exceeded");
+                break;
             default:
                 res.status(500).send("Unexpected error from RTT API: " + api_res.status);
             }
@@ -37,6 +41,31 @@ function fetchRTT(req, res, arrivals) {
             console.error(err);
             res.status(500).send('Failed to fetch from Realtime Trains API');
         });
+}
+
+function trimData(api_res) {
+    return {
+        location: api_res.location.name,
+
+        services: api_res['services']
+            .filter((service) => service.isPassenger && service.locationDetail.isPublicCall)
+            .map((service) => {
+                const arrived = service.locationDetail.realtimeArrivalActual === undefined ?
+                    true : service.locationDetail.realtimeArrivalActual;
+                return {
+                    // Where a service has multiple destinations, return the latest one only.
+                    destination: service.locationDetail.destination.reduce((acc, dest) => {
+                        return dest.workingTime > acc.workingTime ? dest : acc;
+                    }).description,
+
+                    arrived: arrived,
+                    scheduledDepartureTime: parseInt(service.locationDetail.gbttBookedDeparture),
+                    realDepartureTime: parseInt(service.locationDetail.realtimeDeparture),
+                    platform: service.locationDetail.platform,
+                    operator: service.atocName,
+                }
+            }),
+    };
 }
 
 module.exports = router;
